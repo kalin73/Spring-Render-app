30d8e9b557461989bf7a5183877dbe5cdb04101e
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-27 01:45:42 +0200

    init

---
+++ src/main/java/org/example/springrenderapp/SpringRenderAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.springrenderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRenderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRenderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springrenderapp/SpringRenderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrenderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRenderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
